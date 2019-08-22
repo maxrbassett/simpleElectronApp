453a31ab6be0335d24b11132ea186e2afbcfdc6a
Max Bassett <maxrbassett@gmail.com>
2019-08-22 11:40:20 -0600

    Wrapped text in string

--- src/Child.js
+++ src/Child.js
@@ -4,7 +4,7 @@ export default class Child extends React.Component {
     render() {
         return (
             <div className="text-right">
-                <h1>Hello v1.0.4 World!</h1>
+                <h1>{"Hello v1.0.4 World!"}</h1>
                 <p className="ml-4">{"Welcome to your " + this.props.type + "appliation."}</p>
             </div>
         )
