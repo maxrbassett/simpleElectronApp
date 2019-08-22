e3d4a4f8a679bec26696f7b21ac767ff7180d610
Max Bassett <maxrbassett@gmail.com>
2019-08-22 11:54:45 -0600

    Upgrade and remove old code

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
-require('update-electron-app')({
-  repo: 'https://github.com/maxrbassett/simpleElectronApp/'
-});
 const { app, BrowserWindow } = require('electron');
 const { autoUpdater } = require("electron-updater")
 
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
+// // Handle creating/removing shortcuts on Windows when installing/uninstalling.
+// if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+//   app.quit();
+// }
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
